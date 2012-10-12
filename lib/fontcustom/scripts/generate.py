90ae8e1fe74197ac71dfe31beb30eac0dff473e3
Joshua Gross <joshua@gross.is>
2012-10-12 12:01:49 -0400

    Autohint ttf files and spit out svg version of typeface. Ignore generated files.

--- lib/fontcustom/scripts/generate.py
+++ lib/fontcustom/scripts/generate.py
@@ -10,9 +10,13 @@ parser.add_argument('dir', metavar='directory', type=unicode, nargs='+', help='d
 args = parser.parse_args()
 
 f = fontforge.font()
+f.encoding = 'UnicodeFull'
+
 m = md5.new()
 cp = 0xf000
 files = []
+
+KERNING = 15
 
 for dirname, dirnames, filenames in os.walk(args.dir[0]):
 	for filename in filenames:
@@ -25,6 +29,13 @@ for dirname, dirnames, filenames in os.walk(args.dir[0]):
 			glyph = f.createChar(cp)
 			glyph.importOutlines(filePath)
 
+			glyph.left_side_bearing = KERNING
+			glyph.right_side_bearing = KERNING
+
+			# possible optimization?
+			# glyph.simplify()
+			# glyph.round()
+
 			files.append(name)
 			cp += 1
 
@@ -34,9 +45,20 @@ fontfile = args.dir[1] + '/fontcustom-' + hashStr
 f.fontname = 'fontcustom'
 f.generate(fontfile + '.otf',flags=('opentype',))
 f.generate(fontfile + '.ttf')
+f.generate(fontfile + '.svg')
+
+# Fix SVG header for webkit (from: https://github.com/fontello/font-builder/blob/master/bin/fontconvert.py)
+svgfile = open(fontfile + '.svg', 'r+')
+svgtext = svgfile.read()
+svgfile.seek(0)
+svgfile.write(svgtext.replace('''<svg>''', '''<svg xmlns="http://www.w3.org/2000/svg">'''))
+svgfile.close()
 
 scriptPath = os.path.dirname(os.path.realpath(__file__))
 call([scriptPath + '/sfnt2woff', fontfile + '.ttf'])
 call(scriptPath + '/ttf2eot ' + fontfile + '.ttf > ' + fontfile + '.eot', shell=True)
+
+# Hint the TTF file
+call(scriptPath + '/ttfautohint -s -n ' + fontfile + '.ttf ' + fontfile + '-hinted.ttf && mv ' + fontfile + '-hinted.ttf ' + fontfile + '.ttf', shell=True)
 
 print json.dumps({'file': fontfile, 'names': files})
