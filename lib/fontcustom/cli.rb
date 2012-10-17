da57799f5c95da44ee5e5dc5855e8899cf21e8fe
Yifei Zhang <yz@yifei.co>
2012-10-16 22:36:07 -0400

    blocking watcher and cli update

--- lib/fontcustom/cli.rb
+++ lib/fontcustom/cli.rb
@@ -5,8 +5,12 @@ module Fontcustom
   class CLI < Thor
     desc 'compile INPUT_DIR [OUTPUT_DIR]', 'Generates icon webfonts and a corresponding CSS file from a collection of vector images.'
     def compile(input, output = nil)
-      output = output.nil? ? nil : {:output => output}
       Fontcustom.compile(input, output)
+    end
+
+    desc 'watch INPUT_DIR [OUTPUT_DIR]', 'Watches a directory of vector images for changes and regenerates icon webfonts and CSS when there are.'
+    def watch(input, output = nil)
+      Fontcustom.watch(input, output)
     end
   end
 end
--- lib/fontcustom/watcher.rb
+++ lib/fontcustom/watcher.rb
@@ -3,7 +3,10 @@ require 'listen'
 module Fontcustom
   class Watcher
     def self.watch(*args)
-      callback = Proc.new do |modified, added, removed|
+      dir = args.first
+      puts 'Fontcustom is watching your icons at ' + dir
+
+      @listener = Listen.to(dir, :filter => /\.(svg|eps)$/) do |modified, added, removed|
         puts '    >> Changed: ' + modified.join(' ') unless modified.empty?
         puts '    >> Added: ' + added.join(' ') unless added.empty?
         puts '    >> Removed: ' + removed.join(' ') unless removed.empty?
@@ -11,11 +14,6 @@ module Fontcustom
         changed = modified + added + removed
         Fontcustom.compile(*args) unless changed.empty?
       end
-
-      dir = args.first
-      @listener = Listen.to(dir).change(&callback)
-      @listener.start(false)
-      puts 'Fontcustom is watching your icons at ' + dir
     end
 
     def self.stop
