e30ddd6666ac5619080118183237a58e4f998c87
Yifei Zhang <yz@yifei.co>
2013-04-05 15:58:45 -0500

    config file template

--- lib/fontcustom/cli.rb
+++ lib/fontcustom/cli.rb
@@ -1,18 +1,26 @@
 require "thor"
+require "thor/actions"
 require "fontcustom"
 require "fontcustom/watcher"
 
 module Fontcustom
   class CLI < Thor
+    include Thor::Actions
+
     # Actual defaults are stored in Fontcustom::DEFAULT_OPTIONS instead of Thor
     class_option :output, :aliases => "-o", :desc => "The output directory (will be created if it doesn't exist). Default: INPUT/fontcustom/"
-    class_option :config, :aliases => "-c", :desc => "Path to or containing directory of fontcustom.yml (relative to PWD). Default: none" 
+    class_option :config, :aliases => "-c", :desc => "Path to or containing directory of the config file. Default: INPUT/fontcustom.yml" 
     class_option :templates, :aliases => "-t", :type => :array, :desc => "List of templates to compile alongside fonts. Accepts 'css', 'css-ie7', 'scss', 'preview' or arbitrary paths (relative to INPUT or PWD). Default: 'css preview'"
     class_option :font_name, :aliases => "-n", :desc => "The font name used in your templates (automatically normalized to lowercase spinal case). Default: 'fontcustom'"
     class_option :file_hash, :aliases => "-h", :type => :boolean, :desc => "Generate font files with asset-busting hashes. Default: true"
     class_option :css_prefix, :aliases => "-p", :desc => "The prefix for each glyph's CSS class. Default: 'icon-'"
     class_option :debug, :aliases => "-d", :type => :boolean, :desc => "Display debug messages from fontforge. Default: false"
     class_option :verbose, :aliases => "-v", :type => :boolean, :desc => "Display output messages. Default: true"
+
+    # Required for Thor::Actions#template
+    def self.source_root
+      File.join Fontcustom::Util.gem_lib_path, "templates"
+    end
 
     desc "compile [INPUT]", "Generates webfonts and CSS from *.svg and *.eps files in INPUT."
     def compile(input)
@@ -32,6 +40,11 @@ module Fontcustom
       Fontcustom::Watcher.new(opts).watch
     rescue Fontcustom::Error => e
       puts "ERROR: #{e.message}"
+    end
+
+    desc "config [INPUT]", "Adds an annotated fontcustom.yml to INPUT (created if it doesn't exists)."
+    def config(input)
+      template "fontcustom.yml", input 
     end
 
     desc "version", "Shows the version information."
