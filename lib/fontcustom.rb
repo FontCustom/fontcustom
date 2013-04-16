bea95b2459632ded8eebbc117a78b78f57515b31
Yifei Zhang <yz@yifei.co>
2013-04-16 18:34:17 -0500

    module function for cleaner ruby API

--- lib/fontcustom.rb
+++ lib/fontcustom.rb
@@ -4,3 +4,18 @@ require "fontcustom/error"
 require "fontcustom/util"
 require "fontcustom/generator/font"
 require "fontcustom/generator/template"
+
+module Fontcustom
+
+  ##
+  # Clean Ruby API to workaround Thor
+  def compile(options)
+    opts = Fontcustom::Util.collect_options options
+    Fontcustom::Generator::Font.start [opts]
+    Fontcustom::Generator::Template.start [opts]
+  rescue Fontcustom::Error => e
+    puts "ERROR: #{e.message}"
+  end
+
+  module_function :compile
+end
