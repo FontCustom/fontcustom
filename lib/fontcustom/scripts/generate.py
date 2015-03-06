127c473440b9492872132ae17ef3926bea46b3b5
Peter Göbel <peter.goebel@debd.de>
2015-03-06 11:47:10 +0100

    override original fontfile var with regex'd version

--- lib/fontcustom/scripts/generate.py
+++ lib/fontcustom/scripts/generate.py
@@ -97,7 +97,7 @@ try:
         fontfile += '_' + manifest['checksum']['current'][:32]
 
     # Escape brackets
-    re.sub(r'([()])', r'\\\1', fontfile)
+    fontfile = re.sub(r'([()])', r'\\\1', fontfile)
 
     # Generate TTF and SVG
     font.generate(fontfile + '.ttf')
