82ce05940835dadc2f464bab8ece30f44fe3eb51
Alaa Sarhan <sarhan.alaa@gmail.com>
2017-06-26 17:57:25 +0200

    Add single quotes option

--- lib/fontcustom/generator/template.rb
+++ lib/fontcustom/generator/template.rb
@@ -143,30 +143,30 @@ module Fontcustom
         # With and without Base64
         if @options[:base64]
           string = %Q|@font-face {
-  font-family: "#{font_name}";
-  src: #{url}("#{path}.eot?") format("embedded-opentype");
+  font-family: #{quote}#{font_name}#{quote};
+  src: #{url}(#{quote}#{path}.eot?#{quote}) format(#{quote}embedded-opentype#{quote});
   font-weight: normal;
   font-style: normal;
 }
 
 @font-face {
-  font-family: "#{font_name}";
-  src: url("data:application/x-font-woff;charset=utf-8;base64,#{woff_base64}") format("woff"),
-       #{url}("#{path}.woff2") format("woff2"),
-       #{url}("#{path}.ttf") format("truetype"),
-       #{url}("#{path}.svg##{font_name}") format("svg");
+  font-family: #{quote}#{font_name}#{quote};
+  src: url(#{quote}data:application/x-font-woff;charset=utf-8;base64,#{woff_base64}#{quote}) format(#{quote}woff#{quote}),
+       #{url}(#{quote}#{path}.woff2#{quote}) format(#{quote}woff2#{quote}),
+       #{url}(#{quote}#{path}.ttf#{quote}) format(#{quote}truetype#{quote}),
+       #{url}(#{quote}#{path}.svg##{font_name}#{quote}) format(#{quote}svg#{quote});
   font-weight: normal;
   font-style: normal;
 }|
         else
         string = %Q|@font-face {
-  font-family: "#{font_name}";
-  src: #{url}("#{path}.eot");
-  src: #{url}("#{path}.eot?#iefix") format("embedded-opentype"),
-       #{url}("#{path}.woff2") format("woff2"),
-       #{url}("#{path}.woff") format("woff"),
-       #{url}("#{path}.ttf") format("truetype"),
-       #{url}("#{path}.svg##{font_name}") format("svg");
+  font-family: #{quote}#{font_name}#{quote};
+  src: #{url}(#{quote}#{path}.eot#{quote});
+  src: #{url}(#{quote}#{path}.eot?#iefix#{quote}) format(#{quote}embedded-opentype#{quote}),
+       #{url}(#{quote}#{path}.woff2#{quote}) format(#{quote}woff2#{quote}),
+       #{url}(#{quote}#{path}.woff#{quote}) format(#{quote}woff#{quote}),
+       #{url}(#{quote}#{path}.ttf#{quote}) format(#{quote}truetype#{quote}),
+       #{url}(#{quote}#{path}.svg##{font_name}#{quote}) format(#{quote}svg#{quote});
   font-weight: normal;
   font-style: normal;
 }|
@@ -177,8 +177,8 @@ module Fontcustom
 
 @media screen and (-webkit-min-device-pixel-ratio:0) {
   @font-face {
-    font-family: "#{font_name}";
-    src: #{url}("#{path}.svg##{font_name}") format("svg");
+    font-family: #{quote}#{font_name}#{quote};
+    src: #{url}(#{quote}#{path}.svg##{font_name}#{quote}) format(#{quote}svg#{quote});
   }
 }|
         string
@@ -198,7 +198,7 @@ module Fontcustom
 
       def glyph_properties
 %Q|  display: inline-block;
-  font-family: "#{font_name}";
+  font-family: #{quote}#{font_name}#{quote};
   font-style: normal;
   font-weight: normal;
   font-variant: normal;
@@ -213,13 +213,17 @@ module Fontcustom
 
       def glyphs
         output = @glyphs.map do |name, value|
-          %Q|#{@options[:css_selector].sub('{{glyph}}', name.to_s)}#{@pseudo_element} { content: "\\#{value[:codepoint].to_s(16)}"; }|
+          %Q|#{@options[:css_selector].sub('{{glyph}}', name.to_s)}#{@pseudo_element} { content: #{quote}\\#{value[:codepoint].to_s(16)}#{quote}; }|
         end
         output.join "\n"
       end
 
       def pseudo_element
         @pseudo_element
+      end
+
+      def quote
+        @_quote = options[:single_quotes] ? '\'' : '"'
       end
     end
   end
